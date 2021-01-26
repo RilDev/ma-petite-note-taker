480ccc64ed9adbad0acddedbb5f4c060f635e2c4
CodeForFaith <codeforfaith@gmail.com>
2021-01-26 20:37:21 +0100

    add cache delete on activate

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const cacheName = 'v0.0.1';
+const cacheName = 'v0.0.2';
 const filesToCache = [
     './',
     './index.html',
@@ -14,8 +14,23 @@ self.addEventListener('install', event => {
         return cache.addAll(filesToCache);
     }
 
+    // imediately activate the service worker when updated
+    self.skipWaiting();
+
     event.waitUntil(preCache());
 });
+
+// delete outdated cache on cacheName update
+self.addEventListener('activate', event => {
+    const deleteOutdatedCaches = async() => {
+        const keys = await caches.keys();
+        return Promise.all(keys
+            .filter(key => key !== cacheName)
+            .map(key => caches.delete(key)));
+    }
+
+    event.waitUntil(deleteOutdatedCaches());
+})
 
 // serve cached content when offline
 self.addEventListener('fetch', event => {
